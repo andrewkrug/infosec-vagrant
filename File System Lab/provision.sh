7f481cae24d1b0afcf6895b0774de8a8d4f9aa5c
Brandon Kester <brandon@brandonkester.com>
2015-01-19 21:29:04 -0800

    Updated provisioning script for File System Lab.

--- File System Lab/provision.sh
+++ File System Lab/provision.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 # Write a provisioning script
+mkdir -p /fslab/{test,log}
+touch /fslab/log/task.log
+useradd adam
+chmod -R 775 /fslab
+chattr +i /fslab/log
+setfacl -R -m u:adam:--- /fslab/test
